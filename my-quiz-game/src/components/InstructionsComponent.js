8e242646f18da76ed622eec46f84532ca13fbff1
DirtdiverIV <davidgomez.seg@gmail.com>
2024-02-02 23:02:36 +0100

    Added time and progress bar

--- my-quiz-game/src/components/InstructionsComponent.js
+++ my-quiz-game/src/components/InstructionsComponent.js
@@ -3,12 +3,14 @@ import React from 'react';
 
 const InstructionsComponent = () => {
   return (
-    <div className="max-w-md mx-auto p-4 bg-white shadow-lg rounded-md">
-      <h1 className="text-3xl font-bold mb-4">Instructions</h1>
-      <p className="text-lg mb-4">
-        Welcome to the Quiz Game! Read the instructions below and click the "Start" button when you are ready to begin.
-      </p>
-      {/* Agrega cualquier otra información/instrucciones necesarias */}
+    <div className="flex items-center justify-center h-screen">
+      <div className="max-w-2xl p-8 bg-white shadow-lg rounded-md">
+        <h1 className="text-4xl font-bold mb-4">Instructions</h1>
+        <p className="text-lg mb-4">
+          Welcome to the Quiz Game! Read the instructions below and click the "Start" button when you are ready to begin.
+        </p>
+        {/* Agrega cualquier otra información/instrucciones necesarias */}
+      </div>
     </div>
   );
 };
--- my-quiz-game/src/components/Quiz.js
+++ my-quiz-game/src/components/Quiz.js
@@ -1,16 +1,35 @@
-// components/Quiz.js
-import React, { useState } from 'react';
-import '../styles.css'; 
+import React, { useState, useEffect, useRef } from 'react';
+import '../styles.css';
 
 const Quiz = ({ questions }) => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [score, setScore] = useState(0);
   const [showScore, setShowScore] = useState(false);
+  const [timeLeft, setTimeLeft] = useState(10);
+  const timerRef = useRef();
+
+  useEffect(() => {
+    if (timeLeft > 0 && currentQuestion < questions.length && !showScore) {
+      timerRef.current = setInterval(() => {
+        setTimeLeft((prevTime) => prevTime - 1);
+      }, 1000);
+    } else {
+      handleNextQuestion();
+    }
+
+    return () => clearInterval(timerRef.current);
+  }, [currentQuestion, questions, showScore, timeLeft]);
 
   const handleAnswerClick = (isCorrect) => {
     if (isCorrect) {
       setScore(score + 1);
     }
+
+    handleNextQuestion();
+  };
+
+  const handleNextQuestion = () => {
+    setTimeLeft(10);
 
     const nextQuestion = currentQuestion + 1;
 
@@ -25,35 +44,55 @@ const Quiz = ({ questions }) => {
     setCurrentQuestion(0);
     setScore(0);
     setShowScore(false);
+    setTimeLeft(10);
   };
 
+  const progressWidth = ((10 - timeLeft) / 10) * 100;
+
   return (
-    <div className="max-w-md mx-auto p-4 bg-white shadow-lg rounded-md">
-      {showScore ? (
-        <div>
-          <h2 className="text-2xl font-bold mb-4">Your Score: {score} / {questions.length}</h2>
-          <button
-            className="bg-blue-500 text-white px-4 py-2 rounded-md"
-            onClick={resetQuiz}
-          >
-            Start Over
-          </button>
-        </div>
-      ) : (
-        <div>
-          <h2 className="text-xl font-bold mb-2">Question {currentQuestion + 1}:</h2>
-          <p className="text-lg mb-4">{questions[currentQuestion].question}</p>
-          {questions[currentQuestion].answers.map((answer, index) => (
+    <div className="flex items-center justify-center h-screen">
+      <div className="max-w-2xl p-8 bg-white shadow-lg rounded-md">
+        {showScore ? (
+          <div>
+            <h2 className="text-3xl font-bold mb-4">Your Score: {score} / {questions.length}</h2>
             <button
-              key={index}
-              className="bg-blue-500 text-white px-4 py-2 rounded-md mr-2 mb-2"
-              onClick={() => handleAnswerClick(answer.isCorrect)}
+              className="text-lg bg-blue-500 text-white px-4 py-2 rounded-md"
+              onClick={resetQuiz}
             >
-              {answer.text}
+              Start Over
             </button>
-          ))}
-        </div>
-      )}
+          </div>
+        ) : (
+          <div>
+            <h2 className="text-2xl font-bold mb-2">Question {currentQuestion + 1}:</h2>
+            <p className="text-lg mb-4">{questions[currentQuestion].question}</p>
+            <div className="mb-4">
+              <div className="flex relative pt-1">
+                <div className="flex flex-col w-full">
+                  <div
+                    style={{ width: '100%' }}
+                    className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-500"
+                  >
+                    <div
+                      style={{ width: `${progressWidth}%` }}
+                      className="h-2 bg-blue-700"
+                    ></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {questions[currentQuestion].answers.map((answer, index) => (
+              <button
+                key={index}
+                className="text-lg bg-blue-500 text-white px-4 py-2 rounded-md mr-2 mb-2"
+                onClick={() => handleAnswerClick(answer.isCorrect)}
+              >
+                {answer.text}
+              </button>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
