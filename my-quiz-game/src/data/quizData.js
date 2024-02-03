16128f1b17a146a1d6d532e329b50d744083f660
DirtdiverIV <davidgomez.seg@gmail.com>
2024-02-03 12:55:04 +0100

    Added questions about Galicia

--- my-quiz-game/src/data/quizData.js
+++ my-quiz-game/src/data/quizData.js
@@ -1,45 +1,465 @@
 // data/quizData.js
 const quizData = [
-    {
-      question: 'What is the capital of France?',
-      image: 'URL_DE_LA_IMAGEN',
-      answers: [
-        { text: 'Berlin', isCorrect: false },
-        { text: 'Madrid', isCorrect: false },
-        { text: 'Paris', isCorrect: true },
-        { text: 'Rome', isCorrect: false },
-      ],
-    },
-    {
-      question: 'Which planet is known as the Red Planet?',
-      image: 'URL_DE_LA_IMAGEN',
-      answers: [
-        { text: 'Earth', isCorrect: false },
-        { text: 'Mars', isCorrect: true },
-        { text: 'Venus', isCorrect: false },
-        { text: 'Jupiter', isCorrect: false },
-      ],
-    },
-    {
-      question: 'What is the largest mammal in the world?',
-      image: 'URL_DE_LA_IMAGEN',
-      answers: [
-        { text: 'Elephant', isCorrect: false },
-        { text: 'Blue Whale', isCorrect: true },
-        { text: 'Giraffe', isCorrect: false },
-        { text: 'Hippopotamus', isCorrect: false },
-      ],
-    },
-    {
-      question: 'Who wrote "Romeo and Juliet"?',
-      image: 'URL_DE_LA_IMAGEN',
-      answers: [
-        { text: 'Charles Dickens', isCorrect: false },
-        { text: 'Jane Austen', isCorrect: false },
-        { text: 'William Shakespeare', isCorrect: true },
-        { text: 'Emily Brontë', isCorrect: false },
-      ],
-    },
+  {
+    question: '¿Cuál es la capital de Galicia?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Vigo', isCorrect: false },
+      { text: 'A Coruña', isCorrect: false },
+      { text: 'Santiago de Compostela', isCorrect: true },
+      { text: 'Lugo', isCorrect: false },
+    ],
+  },
+  {
+    question: '¿Qué idioma se habla en Galicia además del español?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Catalán', isCorrect: false },
+      { text: 'Euskera', isCorrect: false },
+      { text: 'Gallego', isCorrect: true },
+      { text: 'Inglés', isCorrect: false },
+    ],
+  },
+  {
+    question: '¿Cuál es el plato más famoso de Galicia?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Paella', isCorrect: false },
+      { text: 'Tortilla Española', isCorrect: false },
+      { text: 'Pulpo a la Gallega', isCorrect: true },
+      { text: 'Gazpacho', isCorrect: false },
+    ],
+  },
+  {
+    question: '¿Cuál es la festividad más importante de Galicia?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'San Fermín', isCorrect: false },
+      { text: 'La Tomatina', isCorrect: false },
+      { text: 'El Apóstol Santiago', isCorrect: true },
+      { text: 'Las Fallas', isCorrect: false },
+    ],
+  },
+  {
+    question: '¿Cuál es el río más largo de Galicia?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Río Sil', isCorrect: false },
+      { text: 'Río Miño', isCorrect: true },
+      { text: 'Río Tambre', isCorrect: false },
+      { text: 'Río Ulla', isCorrect: false },
+    ],
+  },
+  {
+    question: '¿Cuál es la catedral más famosa de Galicia?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Catedral de León', isCorrect: false },
+      { text: 'Catedral de Burgos', isCorrect: false },
+      { text: 'Catedral de Santiago de Compostela', isCorrect: true },
+      { text: 'Catedral de Salamanca', isCorrect: false },
+    ],
+  },
+  {
+    question: '¿Cuál es la ciudad más poblada de Galicia?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Santiago de Compostela', isCorrect: false },
+      { text: 'Vigo', isCorrect: true },
+      { text: 'A Coruña', isCorrect: false },
+      { text: 'Lugo', isCorrect: false },
+    ],
+  },
+  {
+    question: '¿Cuál es el vino más famoso de Galicia?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Rioja', isCorrect: false },
+      { text: 'Ribera del Duero', isCorrect: false },
+      { text: 'Albariño', isCorrect: true },
+      { text: 'Cava', isCorrect: false },
+    ],
+  },
+  {
+    question: '¿Cuál es el parque natural más grande de Galicia?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Parque Natural de las Islas Atlánticas', isCorrect: false },
+      { text: 'Parque Natural de las Dunas de Corrubedo', isCorrect: false },
+      { text: 'Parque Natural de O Invernadeiro', isCorrect: true },
+      { text: 'Parque Natural de la Sierra de la Enciña de la Lastra', isCorrect: false },
+    ],
+  },
+  {
+    question: '¿Cuál es el puerto más importante de Galicia?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Puerto de A Coruña', isCorrect: false },
+      { text: 'Puerto de Vigo', isCorrect: true },
+      { text: 'Puerto de Ferrol', isCorrect: false },
+      { text: 'Puerto de Marín', isCorrect: false },
+    ],
+  },
+  {
+    question: '¿Cuál es el instrumento musical tradicional de Galicia?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Guitarra', isCorrect: false },
+      { text: 'Gaita', isCorrect: true },
+      { text: 'Tambor', isCorrect: false },
+      { text: 'Flauta', isCorrect: false },
+    ],
+  },
+  {
+    question: '¿Qué mar baña la costa de Galicia?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Mar Mediterráneo', isCorrect: false },
+      { text: 'Mar Cantábrico', isCorrect: false },
+      { text: 'Océano Atlántico', isCorrect: true },
+      { text: 'Mar Báltico', isCorrect: false },
+    ],
+  },
+  {
+    question: '¿Cuál es el faro más antiguo del mundo que aún está en funcionamiento y se encuentra en Galicia?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Faro de Fisterra', isCorrect: false },
+      { text: 'Faro de Estaca de Bares', isCorrect: false },
+      { text: 'Torre de Hércules', isCorrect: true },
+      { text: 'Faro de Cabo Silleiro', isCorrect: false },
+    ],
+  },
+  {
+    question: '¿Cuál es el nombre del camino de peregrinación que termina en Santiago de Compostela?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Camino de Santiago', isCorrect: true },
+      { text: 'Camino de la Plata', isCorrect: false },
+      { text: 'Camino Francés', isCorrect: false },
+      { text: 'Camino del Norte', isCorrect: false },
+    ],
+  },
+  {
+    question: '¿Cuál es el nombre de la danza tradicional de Galicia?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Flamenco', isCorrect: false },
+      { text: 'Sardana', isCorrect: false },
+      { text: 'Muñeira', isCorrect: true },
+      { text: 'Jota', isCorrect: false },
+    ],
+  },
+  {
+    question: '¿Cuál es el nombre del archipiélago gallego que es Parque Nacional?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Islas Cíes', isCorrect: false },
+      { text: 'Islas Ons', isCorrect: false },
+      { text: 'Islas Atlánticas de Galicia', isCorrect: true },
+      { text: 'Islas Sisargas', isCorrect: false },
+    ],
+  },
+  {
+    question: '¿Cuál es el nombre del licor tradicional de Galicia?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Brandy', isCorrect: false },
+      { text: 'Orujo', isCorrect: true },
+      { text: 'Ron', isCorrect: false },
+      { text: 'Whisky', isCorrect: false },
+    ],
+  },
+  {
+    question: '¿Cuál es el nombre de la torta de almendras típica de Santiago de Compostela?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Tarta de San Marcos', isCorrect: false },
+      { text: 'Tarta de la Abuela', isCorrect: false },
+      { text: 'Tarta de Santiago', isCorrect: true },
+      { text: 'Tarta de Queso', isCorrect: false },
+    ],
+  },
+  {
+    question: '¿Cuál es el nombre del estrecho marítimo que separa Galicia de la vecina Portugal?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Estrecho de Gibraltar', isCorrect: false },
+      { text: 'Estrecho de Magallanes', isCorrect: false },
+      { text: 'Ría de Vigo', isCorrect: false },
+      { text: 'Río Miño', isCorrect: true },
+    ],
+  },
+  {
+    question: '¿Cuál es el nombre de la fortaleza situada en A Coruña que es Patrimonio de la Humanidad?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Castillo de San Felipe', isCorrect: false },
+      { text: 'Castillo de San Antón', isCorrect: false },
+      { text: 'Torre de Hércules', isCorrect: true },
+      { text: 'Castillo de Santa Cruz', isCorrect: false },
+    ],
+  },
+  {
+    question: '¿Cuál es el nombre del famoso camino de peregrinación que pasa por Galicia?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Camino de Santiago', isCorrect: true },
+      { text: 'Camino de la Plata', isCorrect: false },
+      { text: 'Camino Francés', isCorrect: false },
+      { text: 'Camino del Norte', isCorrect: false },
+    ],
+  },
+  {
+    question: '¿Cuál es el nombre del famoso marisco gallego con concha triangular?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Mejillón', isCorrect: false },
+      { text: 'Vieira', isCorrect: true },
+      { text: 'Almeja', isCorrect: false },
+      { text: 'Ostra', isCorrect: false },
+    ],
+  },
+  {
+    question: '¿Cuál es el nombre de la famosa torre situada en A Coruña?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Torre de Pisa', isCorrect: false },
+      { text: 'Torre Eiffel', isCorrect: false },
+      { text: 'Torre de Hércules', isCorrect: true },
+      { text: 'Torre Agbar', isCorrect: false },
+    ],
+  },
+  {
+    question: '¿Cuál es el nombre del famoso licor gallego hecho con restos de uva?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Orujo', isCorrect: true },
+      { text: 'Whisky', isCorrect: false },
+      { text: 'Ron', isCorrect: false },
+      { text: 'Ginebra', isCorrect: false },
+    ],
+  },
+  {
+    question: '¿Cuál es el nombre de la famosa playa situada en las Islas Cíes?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Playa de la Concha', isCorrect: false },
+      { text: 'Playa de las Catedrales', isCorrect: false },
+      { text: 'Playa de Rodas', isCorrect: true },
+      { text: 'Playa de la Malvarrosa', isCorrect: false },
+    ],
+  },
+  {
+    question: '¿Cuál es el nombre del famoso queso gallego con denominación de origen?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Queso Manchego', isCorrect: false },
+      { text: 'Queso Cabrales', isCorrect: false },
+      { text: 'Queso Tetilla', isCorrect: true },
+      { text: 'Queso Idiazábal', isCorrect: false },
+    ],
+  },
+  {
+    question: '¿Cuál es el nombre del famoso pimiento picante gallego?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Pimiento de Padrón', isCorrect: true },
+      { text: 'Pimiento de Guernica', isCorrect: false },
+      { text: 'Pimiento del Piquillo', isCorrect: false },
+      { text: 'Pimiento Choricero', isCorrect: false },
+    ],
+  },
+  {
+    question: '¿Cuál es el nombre del famoso pan gallego con denominación de origen?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Pan de Cea', isCorrect: true },
+      { text: 'Pan de Alfacar', isCorrect: false },
+      { text: 'Pan de Cruz', isCorrect: false },
+      { text: 'Pan de Payés', isCorrect: false },
+    ],
+  },
+  {
+    question: '¿Cuál es el nombre del famoso embutido gallego hecho con partes del cerdo?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Chorizo', isCorrect: false },
+      { text: 'Botillo', isCorrect: false },
+      { text: 'Lacón', isCorrect: true },
+      { text: 'Morcilla', isCorrect: false },
+    ],
+  },
+  {
+    question: '¿Cuál es el nombre del famoso río gallego que desemboca en el Atlántico?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Río Ebro', isCorrect: false },
+      { text: 'Río Duero', isCorrect: false },
+      { text: 'Río Miño', isCorrect: true },
+      { text: 'Río Tajo', isCorrect: false },
+    ],
+  },
+  {
+    question: '¿Cuál es el nombre del reino medieval que se estableció en Galicia?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Reino de León', isCorrect: false },
+      { text: 'Reino de Castilla', isCorrect: false },
+      { text: 'Reino de Navarra', isCorrect: false },
+      { text: 'Reino de Galicia', isCorrect: true },
+    ],
+  },
+  {
+    question: '¿Cuál es el nombre de la cultura pre-romana que habitó Galicia?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Cultura Íbera', isCorrect: false },
+      { text: 'Cultura Tartésica', isCorrect: false },
+      { text: 'Cultura Celta', isCorrect: true },
+      { text: 'Cultura Fenicia', isCorrect: false },
+    ],
+  },
+  {
+    question: '¿Cuál es el nombre del emperador romano nacido en Galicia?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Julio César', isCorrect: false },
+      { text: 'Nerón', isCorrect: false },
+      { text: 'Trajano', isCorrect: false },
+      { text: 'Teodosio I', isCorrect: true },
+    ],
+  },
+  {
+    question: '¿Cuál es el nombre de la revuelta contra el dominio romano en Galicia?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Guerra de las Galias', isCorrect: false },
+      { text: 'Guerras Cántabras', isCorrect: false },
+      { text: 'Guerras Púnicas', isCorrect: false },
+      { text: 'Guerras de Viriato', isCorrect: true },
+    ],
+  },
+  {
+    question: '¿Cuál es el nombre del monasterio medieval más importante de Galicia?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Monasterio de Leyre', isCorrect: false },
+      { text: 'Monasterio de Silos', isCorrect: false },
+      { text: 'Monasterio de Santo Domingo de Silos', isCorrect: false },
+      { text: 'Monasterio de San Martín Pinario', isCorrect: true },
+    ],
+  },
+  {
+    question: '¿Cuál es el nombre del arzobispo que inició la construcción de la catedral de Santiago de Compostela?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Arzobispo Rodrigo', isCorrect: false },
+      { text: 'Arzobispo Diego Gelmírez', isCorrect: true },
+      { text: 'Arzobispo Berenguel de Landoira', isCorrect: false },
+      { text: 'Arzobispo Juan Arias', isCorrect: false },
+    ],
+  },
+  {
+    question: '¿Cuál es el nombre de la batalla en la que Almanzor fue derrotado en Galicia?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Batalla de Clavijo', isCorrect: false },
+      { text: 'Batalla de las Navas de Tolosa', isCorrect: false },
+      { text: 'Batalla de Covadonga', isCorrect: false },
+      { text: 'Batalla de Cervera', isCorrect: true },
+    ],
+  },
+  {
+    question: '¿Cuál es el nombre del primer rey de Galicia?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Rey Alfonso I', isCorrect: false },
+      { text: 'Rey Hermerico', isCorrect: true },
+      { text: 'Rey Rechila', isCorrect: false },
+      { text: 'Rey Miro', isCorrect: false },
+    ],
+  },
+  {
+    question: '¿Cuál es el nombre de la leyenda que explica el origen del Apóstol Santiago en Galicia?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Leyenda de la Dama Blanca', isCorrect: false },
+      { text: 'Leyenda de la Reina Lupa', isCorrect: false },
+      { text: 'Leyenda del Santo Grial', isCorrect: false },
+      { text: 'Leyenda del traslado del cuerpo del Apóstol Santiago', isCorrect: true },
+    ],
+  },
+  {
+    question: '¿Cuál es el nombre del castro gallego más famoso?',
+    image: 'URL_DE_LA_IMAGEN',
+    answers: [
+      { text: 'Castro de Baroña', isCorrect: true },
+      { text: 'Castro de Coaña', isCorrect: false },
+      { text: 'Castro de Chao Samartín', isCorrect: false },
+      { text: 'Castro de Santa Trega', isCorrect: false },
+    ],
+  },
+  {
+    "question": "¿Cuál es la empresa gallega más grande en términos de facturación?",
+    "image": "URL_DE_LA_IMAGEN",
+    "answers": [
+      { "text": "Mercadona", "isCorrect": false },
+      { "text": "Inditex", "isCorrect": true },
+      { "text": "El Corte Inglés", "isCorrect": false },
+      { "text": "Telefónica", "isCorrect": false }
+    ]
+  },
+  {
+    "question": "¿Qué empresa gallega es famosa por su producción de vino albariño?",
+    "image": "URL_DE_LA_IMAGEN",
+    "answers": [
+      { "text": "Bodegas Ramón Bilbao", "isCorrect": false },
+      { "text": "Bodegas Protos", "isCorrect": false },
+      { "text": "Bodegas Martín Códax", "isCorrect": true },
+      { "text": "Bodegas Muga", "isCorrect": false }
+    ]
+  },
+  {
+    "question": "¿Qué empresa gallega es conocida por su producción de conservas de pescado?",
+    "image": "URL_DE_LA_IMAGEN",
+    "answers": [
+      { "text": "Conservas Albo", "isCorrect": false },
+      { "text": "Conservas Frinsa", "isCorrect": true },
+      { "text": "Conservas Dani", "isCorrect": false },
+      { "text": "Conservas Ortiz", "isCorrect": false }
+    ]
+  },
+  {
+    "question": "¿Qué empresa gallega es conocida por su producción de lácteos?",
+    "image": "URL_DE_LA_IMAGEN",
+    "answers": [
+      { "text": "Leche Pascual", "isCorrect": false },
+      { "text": "Leche Asturiana", "isCorrect": false },
+      { "text": "Leche Río", "isCorrect": true },
+      { "text": "Leche Kaiku", "isCorrect": false }
+    ]
+  },
+  {
+    "question": "¿Qué empresa gallega es conocida por su producción de agua mineral?",
+    "image": "URL_DE_LA_IMAGEN",
+    "answers": [
+      { "text": "Aguas de Lanjarón", "isCorrect": false },
+      { "text": "Aguas de Bezoya", "isCorrect": false },
+      { "text": "Aguas de Mondariz", "isCorrect": true },
+      { "text": "Aguas de Solán de Cabras", "isCorrect": false }
+    ]
+  },
+  {
+    "question": "¿Qué empresa gallega es conocida por su producción de cerveza?",
+    "image": "URL_DE_LA_IMAGEN",
+    "answers": [
+      { "text": "Mahou", "isCorrect": false },
+      { "text": "Estrella Galicia", "isCorrect": true },
+      { "text": "San Miguel", "isCorrect": false },
+      { "text": "Cruzcampo", "isCorrect": false }
+    ]
+  }
     // Agrega más preguntas según sea necesario
   ];
   
